3573bb03953146216de5a4f839b286d9482b69e1
Mike Magruder <mikemag@users.noreply.github.com>
2022-05-07 19:30:53 -0700

    Add AutoPrime and Jankbot

---
+++ JankbotCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpBase.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.commands.DefaultDriveCommand;
+import org.firstinspires.ftc.teamcode.commands.DuckBackwardsCommand;
+import org.firstinspires.ftc.teamcode.commands.DuckForwardCommand;
+import org.firstinspires.ftc.teamcode.commands.ExampleCommand;
+import org.firstinspires.ftc.teamcode.subsystems.DuckSpinnerSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.SimpleMecanumSubsystem;
+import org.stealthrobotics.library.AllianceColor;
+
+public abstract class TeleOpBase extends CommandOpMode {
+
+    // Set for us by the red and blue teleop modes
+    final AllianceColor allianceColor;
+
+    // Subsystems
+    SimpleMecanumSubsystem drive;
+    DuckSpinnerSubsystem duckSpinner;
+
+    // Game controllers
+    GamepadEx driveGamepad;
+    GamepadEx mechGamepad;
+
+    @Override
+    public void initialize() {
+        drive = new SimpleMecanumSubsystem(hardwareMap);
+        duckSpinner = new DuckSpinnerSubsystem(hardwareMap, allianceColor);
+
+        driveGamepad = new GamepadEx(gamepad1);
+        mechGamepad = new GamepadEx(gamepad2);
+
+        drive.setDefaultCommand(
+                new DefaultDriveCommand(
+                        drive,
+                        () -> driveGamepad.gamepad.left_stick_y,
+                        () -> driveGamepad.gamepad.left_stick_x,
+                        () -> driveGamepad.gamepad.right_stick_x
+                )
+        );
+
+        driveGamepad.getGamepadButton(GamepadKeys.Button.A).whenPressed(() -> System.out.println("Oh hai"));
+
+        driveGamepad.getGamepadButton(GamepadKeys.Button.X).whileHeld(new DuckForwardCommand(duckSpinner));
+        driveGamepad.getGamepadButton(GamepadKeys.Button.Y).whileHeld(new DuckBackwardsCommand(duckSpinner));
+
+        driveGamepad.getGamepadButton(GamepadKeys.Button.B).whenPressed(new ExampleCommand("I can haz?"));
+//        driveGamepad.getGamepadButton(GamepadKeys.Button.B).whileHeld(new ExampleCommand("I can haz?"));
+//        driveGamepad.getGamepadButton(GamepadKeys.Button.B).whenReleased(new ExampleCommand("I can haz?"));
+    }
+
+    TeleOpBase(AllianceColor allianceColor) {
+        this.allianceColor = allianceColor;
+    }
+
+    // Ideally your red vs. blue opmodes are nothing more than this. Keep code shared between
+    // them, and take different actions based on the alliance color.
+    @SuppressWarnings("unused")
+    @TeleOp(name = "Jankbot RED | Tele-Op", group = "Red")
+    public static class RedTeleOp extends TeleOpBase {
+        public RedTeleOp() {
+            super(AllianceColor.RED);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @TeleOp(name = "Jankbot BLUE | Tele-Op", group = "Blue")
+    public static class BlueTeleOp extends TeleOpBase {
+        public BlueTeleOp() {
+            super(AllianceColor.BLUE);
+        }
+    }
+}
---
+++ JankbotCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SimpleMecanumSubsystem.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class SimpleMecanumSubsystem extends SubsystemBase {
+    public DcMotor leftFrontDrive = null;
+    public DcMotor leftRearDrive = null;
+    public DcMotor rightFrontDrive = null;
+    public DcMotor rightRearDrive = null;
+
+    public SimpleMecanumSubsystem(HardwareMap hardwareMap) {
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        leftRearDrive = hardwareMap.get(DcMotor.class, "leftRearDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        rightRearDrive = hardwareMap.get(DcMotor.class, "rightRearDrive");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightRearDrive.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    public void drive(double leftSickY, double leftStickX, double rightStickX) {
+        // This code is pulled from Game Manual 0
+        // https://gm0.org/en/latest/docs/software/mecanum-drive.html
+
+        double y = -leftSickY; // Remember, this is reversed!
+        double x = leftStickX * 1.1; // Counteract imperfect strafing
+        double rx = rightStickX;
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double leftFrontDrivePower = (y + x + rx) / denominator;
+        double leftRearDrivePower = (y - x + rx) / denominator;
+        double rightFrontDrivePower = (y - x - rx) / denominator;
+        double rightRearDrivePower = (y + x - rx) / denominator;
+
+        leftFrontDrive.setPower(leftFrontDrivePower);
+        leftRearDrive.setPower(leftRearDrivePower);
+        rightFrontDrive.setPower(rightFrontDrivePower);
+        rightRearDrive.setPower(rightRearDrivePower);
+    }
+}
