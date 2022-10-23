711b4645e4e734e64c0ce014a0236d55f2b89908
oliverstivers <oliverstivers@hotmail.com>
2022-10-22 21:00:56 -0700

    updated bot width and added potential auto paths

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,17 +10,37 @@ import com.acmerobotics.roadrunner.*;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 9)
+                .setDimensions(11.75, 13)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(56, -34, Math.toRadians(90)))
-                                .forward(25)
-                                .lineToSplineHeading(new Pose2d(35, -15, Math.toRadians(180)))
-                                .strafeRight(30)
+                        drive.trajectorySequenceBuilder(new Pose2d(37, -65, Math.toRadians(90)))
+                                .lineToLinearHeading(new Pose2d(37, -14, 0))
+                                .forward(20)
+                                .waitSeconds(.5)
+                                .lineToLinearHeading(new Pose2d(30,-8, Math.toRadians(115)))
+                                .waitSeconds(1)
+                                .lineToLinearHeading(new Pose2d(57, -14, 0))
+                                .waitSeconds(.5)
+                                .lineToLinearHeading(new Pose2d(30,-8, Math.toRadians(115)))
+                                .waitSeconds(1)
+                                .lineToLinearHeading(new Pose2d(57, -14, 0))
+                                .waitSeconds(.5)
+                                .lineToLinearHeading(new Pose2d(30,-8, Math.toRadians(115)))
+                                .waitSeconds(1)
+                                .lineToLinearHeading(new Pose2d(57, -14, 0))
+                                .waitSeconds(.5)
+                                .lineToLinearHeading(new Pose2d(30,-8, Math.toRadians(115)))
+                                .waitSeconds(1)
+                                .turn(Math.toRadians(-115))
+                                .strafeRight(3)
+                                .back(90)
+                                .lineToLinearHeading(new Pose2d(-60, -65, Math.toRadians(270)))
                                 .build()
                 );
+
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
