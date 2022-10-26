6d1b60a9a593d0ee6bce0318ac924bb2349ec0f9
Alex Y <yima@rsd407.org>
2022-10-26 15:57:28 -0700

    Working gripper!!!
    Courtesy of Arjun and Quill.

--- XeroCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GripperSubsystem.java
+++ XeroCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GripperSubsystem.java
@@ -15,14 +15,19 @@ public class GripperSubsystem extends SubsystemBase {
     final Servo gripperServo;
 
     public static double CLOSED_POSITION = 0.5;
-    public static double OPEN_POSITION = 0.9;
+    public static double OPEN_POSITION = 0.65;
 
     public GripperSubsystem(HardwareMap hardwareMap) {
         gripperServo = hardwareMap.get(Servo.class, "gripper");
     }
 
-    // TODO: we need methods to open and close the gripper, and then we need to bind them to buttons.
-
+    // TODO: we need methods to open and close the gripper, and then we need to bind them to buttons
+    public void open() {
+        gripperServo.setPosition(OPEN_POSITION);
+    }
+    public void close() {
+        gripperServo.setPosition(CLOSED_POSITION);
+    }
     /**
      * This is called all the time while the opmode is running.
      */
