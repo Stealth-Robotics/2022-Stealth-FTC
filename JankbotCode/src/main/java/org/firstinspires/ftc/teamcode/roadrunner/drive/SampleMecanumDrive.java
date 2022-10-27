64e3cb9b9c8eac5369cbfa08ee2b48095f096909
Clementine Allen <tomtinderbundle@gmail.com>
2022-10-26 18:45:48 -0700

    i hate roadrunner a little less now

--- JankbotCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/SampleMecanumDrive.java
+++ JankbotCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/SampleMecanumDrive.java
@@ -55,8 +55,8 @@ import static org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants.kV;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(6.5, 0.75, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(8, 2, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(8, 2, 0.5);
 
     public static double LATERAL_MULTIPLIER = 1.2245;
 
