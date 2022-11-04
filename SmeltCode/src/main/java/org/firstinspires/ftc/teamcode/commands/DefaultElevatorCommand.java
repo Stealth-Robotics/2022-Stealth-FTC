be5e9ebd11950ffbbddeeef3df5d0afe276755d2
oliverstivers <oliverstivers@gmail.com>
2022-11-04 11:22:44 -0700

    elevator pid?

---
+++ SmeltCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DefaultElevatorCommand.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.subsystems.ElevatorSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.GrabberSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.LiftSubsystem;
+
+import java.util.function.DoubleSupplier;
+
+/**
+ * Spin a wheel forward forever, until the command is cancelled.
+ */
+public class DefaultElevatorCommand extends CommandBase {
+    final ElevatorSubsystem lift;
+
+
+    public DefaultElevatorCommand(ElevatorSubsystem lift) {
+        this.lift = lift;
+        addRequirements(lift);
+    }
+
+    @Override
+    public void execute(){
+        lift.runToPosition();
+    }
+    @Override
+    public boolean isFinished(){
+        return !lift.isBusy();
+    }
+    @Override
+    public void end(boolean interrupted){
+        lift.stop();
+    }
+
+
+}
