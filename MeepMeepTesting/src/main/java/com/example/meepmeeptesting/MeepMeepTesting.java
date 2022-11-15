dcd85782b42de349a2bbba2c41f52a3c3df7ff2b
Clementine Allen <tomtinderbundle@gmail.com>
2022-11-14 16:36:51 -0800

    meep meep paths

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -7,21 +7,27 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(12, 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-30, -64, Math.toRadians(90)))
+                                .lineToSplineHeading(new Pose2d(-34, -43, Math.toRadians(180)))
+                                .lineToSplineHeading(new Pose2d(-38, -12, Math.toRadians(180)))
+                                .forward(18)
+                                .lineToSplineHeading(new Pose2d(-32, -12, Math.toRadians(160)))
+                                .lineToSplineHeading(new Pose2d(-56, -12, Math.toRadians(180)))
+                                .lineToSplineHeading(new Pose2d(-32, -12, Math.toRadians(160)))
+                                .lineToSplineHeading(new Pose2d(-56, -12, Math.toRadians(180)))
+                                .lineToSplineHeading(new Pose2d(-32, -12, Math.toRadians(160)))
+                                .lineToSplineHeading(new Pose2d(-56, -12, Math.toRadians(180)))
+                                .lineToSplineHeading(new Pose2d(-32, -12, Math.toRadians(160)))
+                                .lineToSplineHeading(new Pose2d(-56, -12, Math.toRadians(180)))
+                                .lineToSplineHeading(new Pose2d(-32, -12, Math.toRadians(160)))
+                                .lineToSplineHeading(new Pose2d(-56, -12, Math.toRadians(180)))
                                 .build()
                 );
 
