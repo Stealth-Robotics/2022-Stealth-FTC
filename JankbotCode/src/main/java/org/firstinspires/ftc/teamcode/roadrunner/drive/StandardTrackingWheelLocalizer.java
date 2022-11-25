53931e5e7481a5ab3f52e0989c359931a196693b
Clementine Allen <tomtinderbundle@gmail.com>
2022-11-25 14:50:35 -0800

    re-tuned roadrunner

--- JankbotCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/StandardTrackingWheelLocalizer.java
+++ JankbotCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/StandardTrackingWheelLocalizer.java
@@ -32,7 +32,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     //TODO: Make sure this is correct
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 9.15; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 9.45; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = -4.25; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 1.0187;
