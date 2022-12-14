d73dd47cd9b75422331f919d3842a6859dabb3bc
Mike Magruder <mikemag@users.noreply.github.com>
2022-12-13 17:14:32 -0800

    Dual ground color sensors

--- MikeBotCode/src/main/java/org/firstinspires/ftc/teamcode/commands/AlignToTapeCommand.java
+++ MikeBotCode/src/main/java/org/firstinspires/ftc/teamcode/commands/AlignToTapeCommand.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.commands;
 
-import static org.stealthrobotics.library.opmodes.StealthOpMode.telemetry;
-
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.CommandBase;
 
@@ -20,6 +18,9 @@ public class AlignToTapeCommand extends CommandBase {
 
     public static double ALIGNMENT_SPEED = 0.20;
 
+    boolean lookingForAnyTape = false;
+    boolean done = false;
+
     public AlignToTapeCommand(SimpleMecanumDriveSubsystem drive, GroundSensorSubsystem groundSensor, Direction direction) {
         this.drive = drive;
         this.groundSensor = groundSensor;
@@ -29,14 +30,40 @@ public class AlignToTapeCommand extends CommandBase {
 
     @Override
     public void initialize() {
+        done = false;
+
+        // If we see no tape to start, then start searching for tape given the given hint direction
+        groundSensor.update();
+        boolean lt = groundSensor.leftColorDetected();
+        boolean rt = groundSensor.rightColorDetected();
+        if (!lt && !rt) {
+            lookingForAnyTape = true;
+            if (direction == Direction.RIGHT) {
+                drive.drive(0, ALIGNMENT_SPEED, 0); // Strafe right slowly
+            } else {
+                drive.drive(0, -ALIGNMENT_SPEED, 0); // Strafe left slowly
+            }
+        }
     }
 
     @Override
     public void execute() {
-        double speed = ALIGNMENT_SPEED;
-        if (direction == Direction.LEFT) speed *= -1;
-        telemetry.addData("Alignment speed", speed);
-        drive.drive(0, speed, 0);
+        groundSensor.update();
+        boolean lt = groundSensor.leftColorDetected();
+        boolean rt = groundSensor.rightColorDetected();
+
+        StealthOpMode.telemetry.addData("Left see tape", lt);
+        StealthOpMode.telemetry.addData("Right see tape", rt);
+
+        if (rt) {
+            drive.drive(0, ALIGNMENT_SPEED, 0); // Strafe right slowly
+            lookingForAnyTape = false;
+        } else if (lt) {
+            drive.drive(0, -ALIGNMENT_SPEED, 0); // Strafe left slowly
+            lookingForAnyTape = false;
+        } else {
+            done = !lookingForAnyTape;
+        }
     }
 
     @Override
@@ -46,6 +73,6 @@ public class AlignToTapeCommand extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return groundSensor.centerColorDetected();
+        return done;
     }
 }
--- MikeBotCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GroundSensorSubsystem.java
+++ MikeBotCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/GroundSensorSubsystem.java
@@ -1,36 +1,48 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import static org.stealthrobotics.library.opmodes.StealthOpMode.telemetry;
-
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
 @Config
 public class GroundSensorSubsystem extends SubsystemBase {
-    final RevColorSensorV3 centerSensor;
+    final RevColorSensorV3 leftSensor;
+    final RevColorSensorV3 rightSensor;
 
     public static double TAPE_PRESENT_THRESHOLD = 0.005;
 
+    private NormalizedRGBA leftColors;
+    private NormalizedRGBA rightColors;
+
     public GroundSensorSubsystem(HardwareMap hardwareMap) {
-        centerSensor = hardwareMap.get(RevColorSensorV3.class, "revcsv3");
+        leftSensor = hardwareMap.get(RevColorSensorV3.class, "revcsv3l");
+        rightSensor = hardwareMap.get(RevColorSensorV3.class, "revcsv3r");
     }
 
-    public boolean centerColorDetected() {
-        NormalizedRGBA rgba = centerSensor.getNormalizedColors();
+    public void update() {
+        leftColors = leftSensor.getNormalizedColors();
+        rightColors = rightSensor.getNormalizedColors();
+    }
 
-        return rgba.red > TAPE_PRESENT_THRESHOLD || rgba.blue > TAPE_PRESENT_THRESHOLD;
+    public boolean leftColorDetected() {
+        return leftColors.red > TAPE_PRESENT_THRESHOLD || leftColors.blue > TAPE_PRESENT_THRESHOLD;
+    }
+
+    public boolean rightColorDetected() {
+        return rightColors.red > TAPE_PRESENT_THRESHOLD || rightColors.blue > TAPE_PRESENT_THRESHOLD;
     }
 
     public void periodic() {
-        NormalizedRGBA rgba = centerSensor.getNormalizedColors();
-        telemetry.addData("Red", rgba.red);
-        telemetry.addData("Green", rgba.green);
-        telemetry.addData("Blue", rgba.blue);
-        telemetry.addData("center color detected", centerColorDetected());
+//        update();
+//        telemetry.addData("LR", leftColors.red);
+//        telemetry.addData("LG", leftColors.green);
+//        telemetry.addData("LB", leftColors.blue);
+//        telemetry.addData("RR", rightColors.red);
+//        telemetry.addData("RG", rightColors.green);
+//        telemetry.addData("RB", rightColors.blue);
+//        telemetry.addData("left color detected", leftColorDetected());
+//        telemetry.addData("right color detected", rightColorDetected());
     }
 }
